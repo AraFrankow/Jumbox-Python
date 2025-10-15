07e9409291cb56a2014cd2e723cb7db7a8eb4f5c
marcoskleinn <marcosklein815@gmail.com>
2025-10-15 20:02:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template
+=======
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import sqlite3
+
+#from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
+
+# telefono admin: 12345678
+# contraseña admin: Admin1234
+# telefono usuario1: 87654321
+# contraseña usuario1: Usuario1234
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 
